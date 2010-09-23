30c6fc2a84faf2bfaf93dd99015f3a5dd20b7f22
Arnaud Didry <arnaud.didry@gmail.com>
2010-09-23 18:53:59 +0200

    Initial project structure

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    groupz
+ * @subpackage form
+ * @author     Arnaud Didry <arnaud@didry.info> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
