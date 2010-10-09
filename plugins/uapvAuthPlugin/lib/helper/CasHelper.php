06d95ac2a54cb19c23d2ac4850bede3a0521a019
Arnaud Didry <arnaud@arnaud-laptop.(none)>
2010-10-09 19:51:57 +0200

    Finished CAS autologin

--- plugins/uapvAuthPlugin/lib/helper/CasHelper.php
+++ plugins/uapvAuthPlugin/lib/helper/CasHelper.php
@@ -1,10 +1,24 @@
 <?php
 
+
+/**
+ * return a link to the CAS login page
+ *
+ * @param string $label         content of the link
+ * @param array  $attributes    HTML attributes of the link
+ * @return string               HTML code of the link
+ */
 function link_to_cas_login ($label, $attributes = array())
 {
   return link_to ($label, url_for_cas_login (), $attributes);
 }
 
+/**
+ * Return the absolute URL of the configured CAS server
+ *
+ * @param $url      Return url after successful login
+ * @return string   absolute url
+ */
 function url_for_cas_login ($url = null)
 {
   if ($url === null)
@@ -20,17 +34,18 @@ function url_for_cas_login ($url = null)
   return implode ('/', $casUrl);
 }
 
+/**
+ * Detect CAS session, and force CAS login if possible.
+ * Require activation of the uapvAuthCas module
+ */
 function detect_cas_session ()
 {
-  $iframeUrl = url_for('uapvAuthCAS/detect');
+  $iframeUrl = url_for ('uapvAuthCAS/detect');
+  $casUrl = json_encode (url_for_cas_login ());
 
   return <<<HTML
-
-<script type="text/javascript">
-function uapvAuthCASSessionDetected ()
-{
-  alert ('CAS Detected');
-}</script><iframe src="$iframeUrl" width="1px" height="1px" style="visibility: hidden;"></iframe>
+<script type="text/javascript">function uapvAuthCASSessionDetected () { document.location.href = $casUrl ;}</script>
+<iframe src="$iframeUrl" width="1px" height="1px" style="visibility: hidden;"></iframe>
 HTML
     ;
 }
