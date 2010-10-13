1d5f01fe170dc34b791e91e4f812ee35fed40a16
Arnaud Didry <arnaud.didry@gmail.com>
2010-10-13 23:50:50 +0200

    Email invitations

---
+++ apps/frontend/modules/group_admin/templates/_email_invitation.php
@@ -0,0 +1,25 @@
+<?php slot ('email_subject', _('You have been invited to join a group')) ?>
+
+<p><?php echo __('Hello') ?>,</p>
+
+<p><?php echo __('%user% would like you to join his group "%group_title%"', array (
+    '%user%' => $invitation->getGroup()->getOwner (),
+    '%group_title%' => $invitation->getGroup()
+)) ?></p>
+
+<p><b><?php echo __('Group description') ?></b> :</p>
+
+<p style="background: #FBF8EB; border: 1px solid #EFEBDD; padding: 20px;">
+  <?php echo $invitation->getGroup ()->getDescription () ?>
+</p>
+
+<?php $acceptURL = url_for ('@invitation_accept?invitation='.$invitation->getHash(), true); ?>
+<?php $declineURL = url_for ('@invitation_decline?invitation='.$invitation->getHash(), true); ?>
+
+<p style="color: #4C801E;">
+  <?php echo __('To <b>accept</b> his request open the following link') ?> : <a href="<?php echo $acceptURL ?>"><?php echo $acceptURL ?></a>
+</p>
+
+<p style="color: #B32508;">
+  <?php echo __('Otherwise, open the following link to <b>decline</b>') ?> : <a href="<?php echo $declineURL ?>"><?php echo $declineURL ?></a>
+</p>
---
+++ apps/frontend/templates/_email_signature.php
@@ -0,0 +1,9 @@
+
+
+--
+<p>
+<?php echo _('This email was sent by a robot, do not reply to it, you will not have any response.') ?>
+</p>
+<p>
+  Groupz : <?php echo link_to (url_for ('@homepage', true), '@homepage', array ('absolute' => true)) ?>
+</p>
