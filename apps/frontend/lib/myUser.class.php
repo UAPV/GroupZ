cbb845894ea539cf287b4c1e2ec8686b18341b17
Arnaud Didry <arnaud.didry@gmail.com>
2010-10-14 22:50:47 +0200

    Documented + refactored signIn functions

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -3,38 +3,70 @@
 class myUser extends uapvBasicSecurityUser
 {
 
+  /**
+   * Tells if it's a guest user
+   *
+   * @return boolean
+   */
   public function isGuest ()
   {
     return $this->hasCredential ('guest');
   }
 
+  /**
+   * Tells if it's a trusted user. Someone from the LDAP.
+   *
+   * @return boolean
+   */
   public function isTrusted ()
   {
     return $this->hasCredential ('trusted');
   }
 
   /**
-   * Log guest in
+   * Log a user thanks to its database info
+   *
    * @param User $user
+   * @return void
    */
-  public function signInGuest (User $user)
+  public function signInDbUser (User $user)
   {
-    $this->setAttribute ('user', $user->toArray());
+    $this->setUserObject ($user);
+
+    // configure credentials
+    if ($user->isGuest ())
+      $this->addCredential('guest');
+    else
+      $this->addCredential('trusted');
   }
 
+  /**
+   * Returns the user ID in the database
+   *
+   * @return integer
+   */
   public function getId ()
   {
     $user = $this->getAttribute('user');
     return $user['Id'];
   }
 
+  /**
+   * Returns a text representation of the user (fullname)
+   * 
+   * @return string
+   */
   public function  __toString()
   {
     $user = $this->getAttribute('user');
     return $user['Firstname'].' '.$user['Lastname'];
   }
 
-
+  /**
+   * Configure the User after a CAS login
+   *
+   * @return void
+   */
   public function configure ()
   {
     $userDb = UserQuery::create ()->findOneByEmail ($this->getProfileVar('mail'));
@@ -42,16 +74,12 @@ class myUser extends uapvBasicSecurityUser
     if ($userDb === null)
       $userDb = UserPeer::createFromLdap ($this->getProfile ()->getAll ());
 
-    $this->setUserObject($userDb);
-
-    // configure credentials
-    if ($userDb->isGuest ())
-      $this->addCredential('guest');
-    else
-      $this->addCredential('trusted');
+    $this->signInDbUser ($userDb);
   }
 
   /**
+   * Returns the user object from the database
+   *
    * @return User
    */
   public function getUserObject ()
@@ -59,7 +87,11 @@ class myUser extends uapvBasicSecurityUser
     return UserQuery::create ()->findPk ($this->getId());
   }
 
-
+  /**
+   * Set the current user from a DB object
+   *
+   * @return User
+   */
   public function setUserObject (User $user)
   {
     $this->setAttribute ('user', $user->toArray ());
