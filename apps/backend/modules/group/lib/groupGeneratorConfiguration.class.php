1df7019a597ce80d3f8ef851a370d1c18ed58d1a
Arnaud Didry <arnaud.didry@gmail.com>
2010-10-19 02:24:24 +0200

    Add backend draft

---
+++ apps/backend/modules/group/lib/groupGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * group module configuration.
+ *
+ * @package    groupz
+ * @subpackage group
+ * @author     Arnaud Didry <arnaud@didry.info>
+ * @version    SVN: $Id: configuration.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class groupGeneratorConfiguration extends BaseGroupGeneratorConfiguration
+{
+}
---
+++ apps/backend/templates/layout.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body class="large">
+    <div class="uapv_wrapper">
+      <div class="uapv_header">
+        <h1>
+          Group<span class="_z_">z</span>
+          <span class="tagline">Listes de diffusion et partage de fichiers pour vos groupes de travail</span>
+        </h1>
+      </div>
+      <div class="uapv_content">
+        <?php echo $sf_content ?>
+        <div class="clearboth"></div>
+      </div>
+      <div class="uapv_footer">
+      </div>
+    </div>
+    <div class="uapv_footnote">
+      <p><a href="gpl.univ-avignon.fr">GroupZ - un logiciel libre de l'Université d'Avignon et des Pays de Vaucluse</a></p>
+    </div>
+  </body>
+</html>
