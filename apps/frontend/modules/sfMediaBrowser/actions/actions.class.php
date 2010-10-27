14a5a99194ab4e5e4962b61bf353b3d96ec6b3e4
Arnaud Didry <arnaud.didry@gmail.com>
2010-10-27 02:36:08 +0200

    Override sfMediaBrowser CSS & list template

--- apps/frontend/modules/sfMediaBrowser/actions/actions.class.php
+++ apps/frontend/modules/sfMediaBrowser/actions/actions.class.php
@@ -4,28 +4,24 @@ require_once sfConfig::get('sf_plugins_dir').'/sfMediaBrowserPlugin/modules/sfMe
 
 /**
  *
- *
  * @package     sfMediaBrowser
- * @author      Vincent Agnano <vincent.agnano@particul.es>
+ * @author      Arnaud Didry <arnaud@didry.info>
  */
 class sfMediaBrowserActions extends BasesfMediaBrowserActions
 {
   public function preExecute()
   {
+    $this->group = GroupQuery::create()->findOneByName ($this->getRequestParameter('group_name'));
+    $this->forward404Unless ($this->group);
 
+    // TODO check membership
 
-    // Configured root dir
-    $this->root_dir = sfconfig::get('app_sf_media_browser_root_dir');
+    // Override root dir
+    sfConfig::set('app_sf_media_browser_root_dir', '/uploads/'.$this->group->getName());
+    @mkdir (sfConfig::get('sf_web_dir').'/uploads/'.$this->group->getName());
 
-    // Calculated root path
-    $this->root_path = realpath(sfConfig::get('sf_web_dir').'/'.$this->root_dir);
+    $this->getContext()->getRouting()->setDefaultParameters (array ('group_name' => $this->group->getName()));
 
-    $this->requested_dir = urldecode($this->getRequestParameter('dir'));
-
-    $this->requested_dir = $this->checkPath($this->root_path.'/'.$this->requested_dir)
-                         ? preg_replace('`(/)+`', '/', $this->requested_dir)
-                         : '/';
-
-    $this->getContext()->getRouting()->setDefaultParameters (array ('group_name' => $this->getRequestParameter('group_name')));
+    parent::preExecute();
   }
 }
